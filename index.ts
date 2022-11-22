bef1fd7365caa139e793a7bd7aea8665bd5b9827
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-11-22 09:52:24 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,47 @@
+/**
+ * This is a hourglass program.
+ * By:      Ethan Prieur
+ * Version: 1.0
+ * Since:   2022-11-22
+ */
+
+import promptSync from 'prompt-sync'
+
+/**
+ * The hourGlass() function.
+ *
+ * @param {number} userNum - number from user
+ * @param  {number} spaces - spaces from user
+ */
+function hourGlass (userNum: number, spaces: number = 0): void {
+  let string = ''
+  for (let counter: number = 0; counter < spaces; counter++) {
+    string += ' '
+  }
+  for (let counter1: number = 0; counter1 < userNum; counter1++) {
+    string += '* '
+  }
+  console.log(string + '\n')
+  if (userNum > 1) {
+    hourGlass(userNum - 1, spaces + 1)
+  }
+  console.log(string + '\n')
+}
+
+/**
+ * The main() function.
+ *
+ */
+function main (): void {
+  const prompt = promptSync()
+  const userString = prompt('Enter the hourglass length: ')
+  console.log('')
+  const userNumber = parseInt(userString)
+  if (isNaN(userNumber)) {
+    console.log('Not an number.')
+  } else if (userNumber <= 0) {
+    console.log('That is an invalid number')
+  }
+  hourGlass(userNumber)
+}
+main()
